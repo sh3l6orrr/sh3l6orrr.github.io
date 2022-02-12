5b065793f8f7f908fff4ed1f72846fc9950adbca
Jin Zhang <sh3l6or@icloud.com>
2022-02-12 01:40:29 -0800

    Add some nice properties

--- Maths/Algebra/linear_algebra.js
+++ Maths/Algebra/linear_algebra.js
@@ -1,7 +1,50 @@
 const linearAlgebra = {
-    data() {
-      return {
-        rawHtml: String.raw`
+  data() {
+    return {
+      rawHtml: String.raw`
+        1. By defining a inner product \(<\cdot,\cdot>: V \times V \rightarrow F\) on a vector space \(V\) over \(F\), we can turn it into a inner product space. The inner product should satisfy:
+        <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Property</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>Linearity in 1st argument</td>
+          <td>\(\forall x,y,z \in V,\ forall c \in F: <cx+y,z> = c<x,z>+<y,z>\)</td>
+        </tr>
+        <tr>
+          <td>Positive definite</td>
+          <td>\(\forall x \in V: <x,x> > 0\)</td>
+        </tr>
+        <tr>
+        <td>Conjugate symmetry</td>
+        <td>\(\forall x,y \in V: <x,y> = \overline{<y,x>}\)</td>
+      </tr>
+      </table>
+      2. By defining a norm \(\|\cdot\|: V \rightarrow F\) on a vector space \(V\) over \(F\), we can turn it into a normed space. The norm should satisfy:
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Property</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>Absolute homogeneous</td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td>Positive definite</td>
+          <td></td>
+        </tr>
+        <tr>
+        <td>Satisfy triangle inequality</td>
+        <td></td>
+      </tr>
+      </table>
+      3. A mapping \(f: U \rightarrow V\) between two linear spaces over the field \(F\) may have the following properties:
         <table class="table">
           <thead>
             <tr>
@@ -11,26 +54,18 @@ const linearAlgebra = {
           </thead>
           <tr>
             <td>Homogeneous</td>
-            <td>\(f(cx) = c^n f(x)\)</td>
+            <td>\(\forall x\in U,\ \forall c \in F: f(cx) = c^n f(x)\)</td>
           </tr>
           <tr>
             <td>Linear</td>
-            <td>\(f(cx) = c f(x) \wedge f(x+y) = f(x) + f(y)\)</td>
-          </tr>
-          <tr>
-            <td>Inner product</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>Norm</td>
-            <td></td>
+            <td>\(\forall x\in U,\ \forall c \in F: f(cx) = c f(x) \wedge f(x+y) = f(x) + f(y)\)</td>
           </tr>
         </table>
                 `
-        
-      }
+
     }
   }
+}
 
 Vue.createApp(linearAlgebra).mount('#linear_algebra_')
 
--- Maths/Geometry/topology.js
+++ Maths/Geometry/topology.js
@@ -1,7 +1,8 @@
 const topology = {
-    data() {
-      return {
-        rawHtml: String.raw`
+  data() {
+    return {
+      rawHtml: String.raw`
+                1. For any set, we have the following defined properties:
                 <table class="table">
                   <thead>
                     <tr>
@@ -28,10 +29,56 @@ const topology = {
                     </tr>
                   </tbody>
                 </table>
+                2. If we defined on any set a metric \(d: S \times S \rightarrow \mathbb{R}\), which has to satisfy the following:
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Property</th>
+                      <th scope="col">Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  
+                    <tr>
+                      <td>Symmetric</td>
+                      <td>\(\forall x,y \in S: d(x,y) = d(y,x)\)</td>
+                    </tr>
+                    <tr>
+                      <td>Positive definite</td>
+                      <td>\(\forall x,y \in S: d(x,y) \geqslant 0 \wedge d(x,y) = 0 \rightarrow x=y\)</td>
+                    </tr>
+                    <tr>
+                    <td>Satisfy triangle inequality</td>
+                    <td>\(\forall x,y,z \in S: d(x,z) \leqslant d(x,y) + d(y,z)\)</td>
+                  </tr>
+
+                  </tbody>
+                </table>
+                Then the following properties of its subset \(E \subseteq S) arise:
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Property</th>
+                      <th scope="col">Description</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                    <tr>
+                      <td>Open</td>
+                      <td>\(\forall x \in E,\ \exists r>0: B(x,r) \subseteq S\)</td>
+                    </tr>
+                    <tr>
+                      <td>Closed</td>
+                      <td>\(\forall x \in E^C,\ \exists r>0: B(x,r) \subseteq S\)</td>
+                    </tr>
+
+                  </tbody>
+                </table>
                 `
-        
-      }
+
     }
   }
+}
 
 Vue.createApp(topology).mount('#topology_')
